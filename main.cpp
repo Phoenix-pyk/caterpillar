be10e9ef1e47204317a287e0b628392002e0ba12
Phoenix <161513433+FeeNixPYK@users.noreply.github.com>
2025-06-21 17:47:49 -0400

    Fixed move()

--- main.cpp
+++ main.cpp
@@ -19,61 +19,61 @@ struct dot {
 const int boardwidth = 30; //y coordinate
 const int boardlength = 30; //x coordinate
 
+//Global variables
 char direction = 'D';
 deque <dot> caterpillar;
 bool alive = true;
 
+//function declarations
 void move();
 dot generatefood();
-void checkwall();
+void hitwall();
 
 int main() {
     srand(time(0));
     caterpillar.push_front(dot(3,3));
 
     //Implement gameplay
-
     cout << "code check 1";
     dot leaf = generatefood();
 
     while (alive) {
         //move();
-        //checkwall();
+        //hitwall();
         //if (!alive) break;
-        //checkfood();
-        //checkinput();
+        //eatfood();
+        //inputcheck();
     }
 
     return 0;
 }
 
+//function moves the caterpillar accordingly to user input
 void move() {
     int x = caterpillar[0].x;
     int y = caterpillar[0].y;
+    dot d(0,0);
     if (direction == 'D' || direction == 'd') {
         //move right
-        dot d(x+1,y);
-        caterpillar.push_front(d);
-        caterpillar.pop_back();
+        d.x = x+1;
+        d.y = y;
     }
     else if (direction == 'A' || direction == 'a') {
         //move left
-        dot d(x-1,y);
-        caterpillar.push_front(d);
-        caterpillar.pop_back();
+        d.x = x-1;
+        d.y = y;
     }
     else if (direction == 'S' || direction == 's') {
         //move down
-        dot d(x,y+1);
-        caterpillar.push_front(d);
-        caterpillar.pop_back();
+        d.x = x;
+        d.y = y+1;
     }
     else if (direction == 'W' || direction == 'w') {
         //move up
-        dot d(x,y-1);
-        caterpillar.push_front(d);
-        caterpillar.pop_back();
+        d.x = x;
+        d.y = y-1;
     }
+    caterpillar.push_front(d);
 }
 
 //function make sure food is not generated directly over the caterpillar
@@ -86,10 +86,23 @@ dot generatefood() {
 }
 
 //function check if the caterpillar head hits the wall and end game play if it does
-void checkwall() {
+void hitwall() {
     dot head = caterpillar[0];
     if (head.x>=boardlength || head.x<0 || head.y>=boardwidth || head.y<0) {
         cout <<"YOU DIED... GAME OVER...";
         alive = false;
+        return;
     }
+    for (int i = 1; i<caterpillar.size(); i++) {
+        dot d = caterpillar[i];
+        if (d.x==head.x && d.y==head.y) {
+            cout <<"YOU DIED... GAME OVER...";
+            alive = false;
+            return;
+        }
+    }
+}
+
+void inputcheck() {
+
 }
